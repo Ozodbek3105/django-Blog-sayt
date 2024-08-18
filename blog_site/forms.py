5747143c9b3a167a5f7bf22aaec37bb803c7e623
ozodbek <xudoyberdiyevozodbek04@gmail.com>
2024-08-19 00:23:24 +0500

    kichik kamchiliklar to'g'irlandi

--- blog_site/forms.py
+++ blog_site/forms.py
@@ -9,18 +9,7 @@ class RegistrationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email']
-        widgets = {
-            'username': forms.TextInput(attrs={
-                'class': 'form-control',
-                'placeholder': 'Username',
-                'style': 'width: 300px;',
-            }),
-            'email': forms.EmailInput(attrs={
-                'class': 'form-control',
-                'placeholder': 'Email',
-                'style': 'width: 300px;',
-            }),
-        }
+      
 
     def clean_password2(self):
         password1 = self.cleaned_data.get("password1")
